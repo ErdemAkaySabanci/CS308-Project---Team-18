bbe84f2e1537f02690deec5fc20b79c1b265125d
Öykü <oyku@Oyku-MacBook-Pro.local>
2025-11-14 12:22:41 +0300

    Frontend: add product list/detail pages and update register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import RegisterPage from "./pages/RegisterPage";
+import ProductListPage from "./pages/ProductListPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 function App() {
-  return <RegisterPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/products" element={<ProductListPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+
+        {/* Default olarak ürün listesi */}
+        <Route path="/" element={<ProductListPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
